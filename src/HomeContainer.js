61894fc3692dbc0e41a57f2d9c52025fc3ca8c7e
Brennan Blair <bblair31@users.noreply.github.com>
2019-01-28 20:54:45 -0500

    Add sorting to upcoming details birthdays

--- src/HomeContainer.js
+++ src/HomeContainer.js
@@ -31,7 +31,6 @@ class HomeContainer extends Component {
   }
 
   render(){
-    console.log(this.sidebarDates())
     return(
       <div className="ui grid">
         <Sidebar sidebarDates={this.sidebarDates()}/>
